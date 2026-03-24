36477d00d0e743cf46b99a16899b7259187054eb
Victor Stoica <113309128+victorstoica114@users.noreply.github.com>
2026-03-24 13:13:11 +0200

    Add Deye BMS protocol task header file

---
+++ main/Protocol_Tasks/Deye/deye_bms_task.h
@@ -0,0 +1,12 @@
+// Deye BMS Protocol Tasks Header
+
+#ifndef DEYE_BMS_TASK_H
+#define DEYE_BMS_TASK_H
+
+// Function prototypes for Deye BMS tasks
+
+void initDeyeBMSTask();
+void startDeyeBMSTask();
+void stopDeyeBMSTask();
+
+#endif // DEYE_BMS_TASK_H
